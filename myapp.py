329286db6676d87837284bb6462348411625ce9b
jianpingy <60832776+jianpingy@users.noreply.github.com>
2025-11-18 10:41:34 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -290,4 +290,4 @@ with gr.Blocks() as demo:
 # Launch the app
 # --------------------------------------------------
 if __name__ == "__main__":
-    demo.launch(share=True, server_name="0.0.0.0", server_port=7860, inbrowser=True)
+    demo.launch(share=True)
